2f5c3a4d67782dedd75a7650d4b3491bdb0475b7
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-05-29 21:12:01 -0300

    adicionado o call validator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,22 @@
-import { Controller, Get, Post,Body } from '@nestjs/common';
+import { Controller, Get, Post,Body, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
 
    constructor(private readonly userService:UserService){}
 
+   @UsePipes(ValidationPipe)
     @Post()
     async createUsers(@Body() createUserDto:CreateUserDto):Promise<UserEntity>{
         return this.userService.createUser(createUserDto)
      }
 
        @Get()
-    async getAllUsers():Promise<UserEntity[]>{
-       return this.userService.getAllUser()
+    async getAllUsers():Promise<ReturnUserDto[]>{
+       return (await this.userService.getAllUser()).map((userEntity) => new ReturnUserDto(userEntity))
     }
 }
