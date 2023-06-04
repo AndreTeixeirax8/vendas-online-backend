915e654f0ecf82eabb5aadf4698afcb19d1b4080
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-06-04 12:10:52 -0300

    adicionado validacao de cidade e user

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,NotFoundException  } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CacheService } from 'src/cache/cache.service';
 import { Repository } from 'typeorm';
 import { CityEntity } from './entities/city.entity';
 
+@Injectable()
 export class CityService {
   constructor(
     @InjectRepository(CityEntity)
@@ -22,5 +23,19 @@ export class CityService {
       }),
     );
 
+  }
+
+  async findCityById(cityId:number):Promise<CityEntity>{
+    const city = await this.cityRepository.findOne({
+      where:{
+        id:cityId
+      }
+    })
+
+    if(!city){
+      throw new NotFoundException('Cidade invalida')
+    }
+
+    return city
   }
 }
