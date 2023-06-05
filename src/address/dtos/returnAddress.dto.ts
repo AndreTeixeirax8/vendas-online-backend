50265d4af827e01fb6f17392bca5385128c20b7c
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-06-05 20:51:31 -0300

    ajuste nos dtos

--- src/address/dtos/returnAddress.dto.ts
+++ src/address/dtos/returnAddress.dto.ts
@@ -1,16 +1,18 @@
 import { IsInt, IsOptional, IsString } from "class-validator";
 import { AddressEntity } from "../entities/address.entity";
+import { ReturnCityDto } from "src/city/dtos/returnCity.dto";
 
 export class ReturnAddressDto{
     complement:string;
     number:number;
     cep:string;
-    //city?:any;
+    city?:ReturnCityDto;
 
     constructor(address:AddressEntity){
         this.complement = address.complement;
         this.number = address.number;
         this.cep = address.cep;
+        this.city = address.city ? new ReturnCityDto(address.city) :undefined;
 
 
     }
