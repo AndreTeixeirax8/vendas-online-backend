66ca4ae5e37935bc4119b720a74e2f3adca24edc
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-06-06 20:08:35 -0300

    iniciado modulo de autenticacao

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -57,4 +57,17 @@ export class UserService {
         return user;
 
     }
+
+    async findUserByEmail(email:string):Promise<UserEntity>{
+        const user = await this.userRepository.findOne({
+            where:{email}
+        });
+
+        if(!user){
+            throw new NotFoundException(`E-mail não encontrado`)
+        }
+
+        return user;
+
+    }
 }
