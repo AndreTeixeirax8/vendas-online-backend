c03af1221d92821a20f6140a95f57f3986bb9095
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-06-12 21:16:08 -0300

    adicionado busca endereco por id user

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
@@ -6,6 +6,7 @@ import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
 import { UserId } from 'src/decorators/use-id.decorator';
 
+@Roles(UserType.ADMIN)
 @Controller('address')
 export class AddressController {
 
@@ -13,7 +14,7 @@ export class AddressController {
         private readonly addressService:AddressService
     ){}
 
-    @Roles(UserType.ADMIN)
+   // @Roles(UserType.ADMIN)
     @Post()
     @UsePipes(ValidationPipe)
     async createAddress(
@@ -22,6 +23,13 @@ export class AddressController {
        @UserId()userId:number
         ):Promise<AddressEntity>{
         return  this.addressService.createAddress(createAddressDto,userId)
+    }
+
+
+    
+    @Get()
+    async findAddressByUserId(@UserId()userId:number):Promise<AddressEntity[]>{
+        return  this.addressService.findAddressByUserId(userId)
     }
 
 }
