75b0aad1390fab5637317e1bcc65c3757445b419
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-06-17 09:05:55 -0300

    adicionado o metodo de atualizacao de produtos

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get,UsePipes,ValidationPipe,Post,Body,Delete,Param } from '@nestjs/common';
+import { Controller, Get,UsePipes,ValidationPipe,
+  Post,Body,Delete,Param,Put } from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
 import { ReturnProduct } from './dtos/return-product.dto';
@@ -6,6 +7,7 @@ import { ProductService } from './product.service';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { ProductEntity } from './entities/product.entity';
 import { DeleteResult} from 'typeorm';
+import { UpdateProductDTO } from './dtos/update-product.dto';
 
 @Roles(UserType.ADMIN, UserType.USER)
 @Controller('product')
@@ -34,5 +36,15 @@ export class ProductController {
     @Param('productId') productId: number,
   ): Promise<DeleteResult> {
     return this.productService.deleteProduct(productId);
+  }
+
+  @Roles(UserType.ADMIN)
+  @UsePipes(ValidationPipe)
+  @Put('/:productId')
+  async updateProduct(
+    @Body() updateProduct: UpdateProductDTO,
+    @Param('productId') productId: number,
+  ): Promise<ProductEntity> {
+    return this.productService.updateProduct(updateProduct, productId);
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -4,6 +4,7 @@ import { DeleteResult, Repository } from 'typeorm';
 import { ProductEntity } from './entities/product.entity';
 import { CategoryService } from 'src/category/category.service';
 import { CreateProductDTO } from './dtos/create-product.dto';
+import { UpdateProductDTO } from './dtos/update-product.dto';
 
 @Injectable()
 export class ProductService {
@@ -50,5 +51,17 @@ export class ProductService {
     await this.findProductById(productId);
 
     return this.productRepository.delete({ id: productId });
+  }
+
+  async updateProduct(
+    updateProduct: UpdateProductDTO,
+    productId: number,
+  ): Promise<ProductEntity> {
+    const product = await this.findProductById(productId);
+
+    return this.productRepository.save({
+      ...product,
+      ...updateProduct,
+    });
   }
 }
