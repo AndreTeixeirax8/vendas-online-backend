b0853231e0697cce91fa159d59a4a95c0100a2f7
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-06-18 12:37:06 -0300

    adicionado insert do carrinho

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -14,6 +14,9 @@ import {
   
     @Column({ name: 'user_id', nullable: false })
     userId: number;
+
+    @Column({ name: 'active', nullable: false })
+    active: boolean;
   
     @CreateDateColumn({ name: 'created_at' })
     createdAt: Date;
