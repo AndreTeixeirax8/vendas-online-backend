858d6aa62dbbfd3c8352f349e25fa9314afb449a
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-06-28 19:16:34 -0300

    criacao da logica de pagamento parte 2

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,15 +3,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { CreateOrderDTO } from './create-order.dto';
+import { PaymentService } from 'src/payment/payment.service';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(OrderEntity)
     private readonly orderEntity: Repository<OrderEntity>,
+    private readonly paymentService: PaymentService,
   ) {}
 
   async createOrder(createOrderDTO: CreateOrderDTO, cartId: number) {
+    await this.paymentService.createPayment(createOrderDTO);
     return null;
   }
 }
