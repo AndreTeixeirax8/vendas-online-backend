974d1d35cfff9c0624a54aae1aa1707c837893a1
AndreTeixeirax8 <andre-x8@hotmail.com>
2023-07-07 19:39:12 -0300

    adicionado dtos

--- src/payment/dtos/return-payment.dto.ts
+++ src/payment/dtos/return-payment.dto.ts
@@ -1,3 +1,4 @@
+import { ReturnPaymentStatus } from 'src/payment-status/dtos/return-payment-status.dto';
 import { PaymentStatusEntity } from '../../payment-status/entities/payment-status.entity';
 import { PaymentEntity } from '../entities/payment.entity';
 
@@ -8,7 +9,7 @@ export class ReturnPaymentDTO {
   discount: number;
   finalPrice: number;
   type: string;
-  paymentStatus?: PaymentStatusEntity;
+  paymentStatus?: ReturnPaymentStatus;
 
   constructor(payment: PaymentEntity) {
     this.id = payment.id;
@@ -17,6 +18,8 @@ export class ReturnPaymentDTO {
     this.discount = payment.discount;
     this.finalPrice = payment.finalPrice;
     this.type = payment.type;
-    this.paymentStatus = payment.paymentStatus;
+    this.paymentStatus = payment.paymentStatus
+    ? new ReturnPaymentStatus(payment.paymentStatus)
+    : undefined;
   }
 }
